97405818271eefa109ee76c4bbc57e154f1a1668
alxshbln <alxshbln@gmail.com>
2021-03-11 22:06:04 +0200

    Добавил префиксы, минифицировал, завершил проект

--- js/script.js
+++ js/script.js
@@ -6,6 +6,55 @@ const dateOut = formPopup.querySelector(".out-label-text");
 const olderNumbers = formPopup.querySelector(".older-label-numbers");
 const kidsNumbers = formPopup.querySelector(".kids-label-numbers");
 
+const minusOlderBtn = formPopup.querySelector(".older-number-minus");
+const plusOlderBtn = formPopup.querySelector(".older-number-plus");
+const minusKidsBtn = formPopup.querySelector(".kids-number-minus");
+const plusKidsBtn = formPopup.querySelector(".kids-number-plus");
+
+let olderCounter = Number(olderNumbers.value);
+let kidsCounter = Number(kidsNumbers.value);
+console.log(olderCounter);
+
+const minusFuncOlder = (e) => {
+  e.preventDefault();
+  olderCounter -= 1;
+  olderNumbers.value = olderCounter;
+  if (olderCounter <= 0) {
+    minusOlderBtn.removeEventListener("click", minusFuncOlder);
+  }
+};
+
+const minusFuncKids = (e) => {
+  e.preventDefault();
+  kidsCounter -= 1;
+  kidsNumbers.value = kidsCounter;
+  if (kidsCounter <= 0) {
+    minusKidsBtn.removeEventListener("click", minusFuncKids);
+  }
+};
+
+minusOlderBtn.addEventListener("click", minusFuncOlder);
+
+plusOlderBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  olderCounter += 1;
+  olderNumbers.value = olderCounter;
+  console.log(olderCounter);
+});
+
+minusKidsBtn.addEventListener("click", minusFuncKids);
+
+plusKidsBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  kidsCounter += 1;
+  kidsNumbers.value = kidsCounter;
+  console.log(kidsCounter);
+});
+
+// olderNumbers.value = 10;
+
+formPopup.classList.remove("fallback");
+
 let isStorageSupport = true;
 let storage = "";
 
@@ -28,7 +77,12 @@ appointmentButton.addEventListener("click", function (evt) {
 });
 
 travelForm.addEventListener("submit", function (evt) {
-  if (!dateIn.value || !dateOut.value || !olderNumbers.value || !kidsNumbers.value) {
+  if (
+    !dateIn.value ||
+    !dateOut.value ||
+    !olderNumbers.value ||
+    !kidsNumbers.value
+  ) {
     evt.preventDefault();
     formPopup.classList.remove("modal-error");
     formPopup.offsetWidth = formPopup.offsetWidth;
@@ -37,7 +91,7 @@ travelForm.addEventListener("submit", function (evt) {
     if (isStorageSupport) {
       localStorage.setItem("older", olderNumbers.value);
       localStorage.setItem("kids", kidsNumbers.value);
-    }  
+    }
   }
 });
 
